a367fff706e0a56a891d33998ac9f1a395dfb203
killerFeature <jens.schwarzer@gmail.com>
2019-01-10 14:37:34 +0000

    new commit

---
+++ deployManagedApp.sh
@@ -0,0 +1,26 @@
+zip ManagedApp.zip createUiDefinition.json mainTemplate.json
+git add .
+git commit -m "new commit"
+git push origin master
+
+
+#Deploy
+# Create resource group
+az group create --name appDefinitionGroup --location northeurope
+
+# Get Azure Active Directory group to manage the application
+groupid=$(az ad group show --group appManagers --query objectId --output tsv)
+
+# Get role
+roleid=$(az role definition list --name Owner --query [].name --output tsv)
+
+# Create the definition for a managed application
+az managedapp definition create \
+  --name "ManagedStorage" \
+  --location "northeurope" \
+  --resource-group appDefinitionGroup \
+  --lock-level ReadOnly \
+  --display-name "Managed Storage Account" \
+  --description "Managed Azure Storage Account" \
+  --authorizations "$groupid:$roleid" \
+  --package-file-uri "https://github.com/KillerFeature/ManagedAppDemo/raw/master/ManagedApp.zip"
