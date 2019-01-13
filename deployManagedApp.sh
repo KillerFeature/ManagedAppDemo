5ed5b2b42aaa9d85d339d81d254dd99477176c2a
killerFeature <jens.schwarzer@gmail.com>
2019-01-13 12:27:17 +0000

    New commit

--- deployManagedApp.sh
+++ deployManagedApp.sh
@@ -9,13 +9,13 @@ git push origin master
 # Create resource group
 az group create --name appDefinitionGroup --location northeurope
 
-# Get Azure Active Directory group to manage the application
+# Get Azure Active Directory group to manage the application (you need to replace ManagedAppAdmins23462125 with your own group)
 groupid=$(az ad group show --group ManagedAppAdmins23462125 --query objectId --output tsv)
 roleid=$(az role definition list --name Owner --query [].name --output tsv)
 
 # Create the definition for a managed application
 az managedapp definition create \
-  --name "ManagedStorage03" \
+  --name "ManagedStorage04" \
   --location "northeurope" \
   --resource-group appDefinitionGroup \
   --lock-level ReadOnly \
